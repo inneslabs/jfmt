d9b0848aa345293ef2fe0575293fd09cc3779c2a
Joey Innes <avianpneuma@gmail.com>
2024-02-12 21:15:07 +0100

    FIx the crap

--- fmt.go
+++ fmt.go
@@ -1,28 +1,29 @@
-package jfmt
+package report
 
 import "fmt"
 
-func FmtCount(count uint32) string {
-	const unit = 1000
+func FmtCount32(count uint32) string {
+	const unit = uint32(1000)
 	if count < unit {
 		return fmt.Sprintf("%d", count)
 	}
-	div, exp := uint32(unit), 0
+	div, exp := unit, 0
 	// 1000, 1000*1000, 1000*1000*1000, 1000*1000*1000*1000
 	for n := count / unit; n >= unit; n /= unit {
 		div *= unit
 		exp++
 	}
 	return fmt.Sprintf("%.1f%c",
-		float32(count)/float32(div), "KMGTPE"[exp])
+		float32(count)/float32(div),
+		"KMGTPE"[exp])
 }
 
-func FmtFileSize(size int64) string {
-	const unit = 1024
+func FmtSize64(size uint64) string {
+	const unit = uint64(1024)
 	if size < unit {
 		return fmt.Sprintf("%dB", size)
 	}
-	div, exp := uint32(unit), 0
+	div, exp := unit, 0
 	// 1024, 1024*1024, 1024*1024*1024, 1024*1024*1024*1024
 	for n := size / unit; n >= unit; n /= unit {
 		div *= unit

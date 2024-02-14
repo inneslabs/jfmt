a3273af54fe6b7a4f7c76bde4e52ac0b621563d1
Joey Innes <avianpneuma@gmail.com>
2024-02-14 09:23:15 +0100

    Add FmtDuration

--- fmt.go
+++ fmt.go
@@ -1,6 +1,10 @@
 package jfmt
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func FmtCount32(count uint32) string {
 	const unit = uint32(1000)
@@ -31,4 +35,28 @@ func FmtSize64(size uint64) string {
 	}
 	return fmt.Sprintf("%.1f%cB",
 		float64(size)/float64(div), "KMGTPE"[exp])
+}
+
+// FmtDuration formats a time.Duration into a shorter string representation.
+func FmtDuration(d time.Duration) string {
+	var parts []string
+
+	hours := d / time.Hour
+	d -= hours * time.Hour
+	if hours > 0 {
+		parts = append(parts, fmt.Sprintf("%dh", hours))
+	}
+
+	minutes := d / time.Minute
+	d -= minutes * time.Minute
+	if minutes > 0 {
+		parts = append(parts, fmt.Sprintf("%dm", minutes))
+	}
+
+	seconds := d / time.Second
+	if seconds > 0 {
+		parts = append(parts, fmt.Sprintf("%ds", seconds))
+	}
+
+	return strings.Join(parts, "")
 }
